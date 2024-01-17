4985fbeafaf08b7c679a5cb29400787d5ab8a50f
Francisco Cannepa <fran981942@hotmail.com>
2024-01-17 00:19:49 -0300

    Delete lib/Db/dbConstants.dart

--- lib/Db/dbConstants.dart
+++
@@ -1,5 +0,0 @@
-/* const DB_URL = "mongodb+srv://fcannepaborges:qrenn4hlap97b49yrld0@petfindr.bkpjmn1.mongodb.net/?retryWrites=true&w=majority"; */
-const DB_URL = 'mongodb+srv://fcannepaborges:qrenn4hlap97b49yrld0@petfindr.bkpjmn1.mongodb.net/PetFindr?retryWrites=true&w=majority';
-
-const REPORTS_COLLECTION = "Reports";
-const USERS_COLLECTION = "Users";
